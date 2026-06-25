4a73b3143774f2a4c207bc2fef7bd8a8fa34cfaa
Brian <brianpasco1206@gmail.com>
2026-06-25 23:37:34 +0800

    fix(App): build error from scaffold XAML files with no appropriate code setup

---
+++ RescuAR.App/Views/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using RescuAR.App.Services.Unity;
+namespace RescuAR.App.Views;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void LaunchUnityClicked(
+        object? sender,
+        EventArgs e)
+    {
+#if ANDROID
+        var unityService =
+            Handler?.MauiContext?.Services.GetService<IUnityService>();
+
+        unityService?.LaunchUnity();
+#endif
+    }
+} 
