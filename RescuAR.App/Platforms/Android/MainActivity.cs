01b43a9c21eb5cc0db676689b1dd018c4f129929
Brian <brianpasco1206@gmail.com>
2026-07-30 17:09:24 +0800

    refactor(Platforms): revert MainActivity.cs state to prod

--- RescuAR.App/Platforms/Android/MainActivity.cs
+++ RescuAR.App/Platforms/Android/MainActivity.cs
@@ -1,30 +1,11 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
-using RescuAR.App.Platforms.Android.Unity;
 
-namespace RescuAR.App.Platforms.Android;
-
-[Activity(
-    Theme = "@style/Maui.SplashTheme",
-    MainLauncher = true,
-    LaunchMode = LaunchMode.SingleTop,
-    ConfigurationChanges =
-        ConfigChanges.ScreenSize |
-        ConfigChanges.Orientation |
-        ConfigChanges.UiMode |
-        ConfigChanges.ScreenLayout |
-        ConfigChanges.SmallestScreenSize |
-        ConfigChanges.Density)]
-public class MainActivity : MauiAppCompatActivity
+namespace RescuAR.App
 {
-    protected override void OnCreate(
-        Bundle? savedInstanceState)
+    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    public class MainActivity : MauiAppCompatActivity
     {
-        base.OnCreate(savedInstanceState);
-
-        UnityVerifier.Verify();
-
-        NativeLibraryVerifier.Verify();
     }
 }
