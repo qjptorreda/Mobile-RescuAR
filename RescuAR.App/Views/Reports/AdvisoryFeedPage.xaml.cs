0c07c442a21ee31d977853cfebcdb5855e3d4336
Aubrey Tavu <qadctavu@tip.edu.ph>
2026-08-03 15:41:10 +0800

    Add AdvisoryFeedPage with ViewModel binding

---
+++ RescuAR.App/Views/Reports/AdvisoryFeedPage.xaml.cs
@@ -0,0 +1,14 @@
+using Microsoft.Maui.Controls;
+using RescuAR.App.ViewModels.Reports;
+
+namespace RescuAR.App.Views.Reports
+{
+    public partial class AdvisoryFeedPage : ContentPage
+    {
+        public AdvisoryFeedPage()
+        {
+            InitializeComponent();
+            BindingContext = new AdvisoryFeedViewModel();
+        }
+    }
+}
