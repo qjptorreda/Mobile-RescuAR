015b0972833f029c92c0a73c23cfd0dcc03f1c2b
Aubrey Tavu <qadctavu@tip.edu.ph>
2026-08-06 19:19:45 +0800

    Register new routes for AdvisoryFeed and SafetyCircle

--- RescuAR.App/AppShell.xaml.cs
+++ RescuAR.App/AppShell.xaml.cs
@@ -11,14 +11,11 @@ public partial class AppShell : Shell
         Routing.RegisterRoute("Prepare/Assessment", typeof(Views.Prepare.AssessmentPage));
         Routing.RegisterRoute("Prepare/EvacuationCenterInfo", typeof(Views.Prepare.EvacuationCenterInfoPage));
         Routing.RegisterRoute("Prepare/HotlineDirectory", typeof(Views.Prepare.EvacuationCenterInfoPage));
+        Routing.RegisterRoute("AdvisoryFeedPage", typeof(Views.Reports.AdvisoryFeedPage));
+        Routing.RegisterRoute("Reports/AdvisoryFeed", typeof(Views.Reports.AdvisoryFeedPage));
         Routing.RegisterRoute("ProfilePage", typeof(Views.Profile.ProfilePage));
-        Routing.RegisterRoute("Reports/CommunityPosting", typeof(Views.Reports.CommunityPostingPage));
         Routing.RegisterRoute("ReportDetails", typeof(Views.Reports.ReportDetailsPage));
-        
-        // Summary & Advisory Routes (registered to both Dashboard & Reports/Summary folders)
-        Routing.RegisterRoute("SummaryPage", typeof(Views.Dashboard.SummaryPage));
-        Routing.RegisterRoute("AreaStatusSummaryPage", typeof(Views.Dashboard.SummaryPage));
-        Routing.RegisterRoute("AdvisoryFeedPage", typeof(Views.Dashboard.AdvisoryFeedPage));
-        Routing.RegisterRoute("Reports/AdvisoryFeed", typeof(Views.Dashboard.AdvisoryFeedPage));
+        Routing.RegisterRoute("SafetyCirclePage", typeof(Views.Map.SafetyCirclePage));
+        Routing.RegisterRoute("SummaryPage", typeof(Views.Summary.SummaryPage));
     }
 }
